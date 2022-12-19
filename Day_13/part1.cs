2b28a599841f3c12c9e6c502a82d9f21c2bf29c4
mariru27 <marina.rusu@caphyon.com>
2022-12-19 13:17:27 +0200

    Remove unused code from day 13 part 1. Bugfix: check if is list in another way

--- Day_13/part1.cs
+++ Day_13/part1.cs
@@ -1,37 +1,8 @@
-using System.Text.RegularExpressions;
-
 const string inputPath = @"C:\Users\Marina\source\repos\aoc-2022\aoc-2022\input.txt";
 string[] lines = File.ReadAllLines(inputPath);
-//7413
-string GetFirstDigit(string tempList)
-{
-  MatchCollection matches = Regex.Matches(tempList, @"\d+");
-  string[] resultFirst = matches.Cast<Match>()
-                         .Take(1)
-                         .Select(match => match.Value)
-                         .ToArray();
-  //if (resultFirst.Any())
-  return resultFirst[0];
-  //return "-1";
-}
-
-int CountNumbersInString(string line)
-{
-  MatchCollection matches = Regex.Matches(line, @"\d+");
-  string[] resultFirst = matches.Cast<Match>()
-                         .Select(match => match.Value)
-                         .ToArray();
-  return resultFirst.Length;
-}
-
-bool IsDigit(char c)
-{
-  return c >= '0' && c <= '9';
-}
 
 KeyValuePair<string, string> GetNextElement(string list)
 {
-
   int nr = 0, j = 0;
   string result = "";
   while (j < list.Length)
@@ -61,7 +32,7 @@ KeyValuePair<string, string> GetNextElement(string list)
 
   KeyValuePair<string, string> keyValuePair2;
   if (result.Length + 1 < list.Length)
-  { 
+  {
     keyValuePair2 = new(result, list.Substring(result.Length + 1));
   }
   else
@@ -73,71 +44,6 @@ KeyValuePair<string, string> GetNextElement(string list)
   return keyValuePair2;
 }
 
-
-Result CompareList(string list1, string list2)
-{
-  List<int> resultList1 = new List<int>();
-  List<int> resultList2 = new List<int>();
-  if (list1.Contains(','))
-  {
-    var res = list1.Split(',');
-    foreach (string item in res)
-    {
-      resultList1.Add(int.Parse(item));
-    }
-  }
-  else
-  {
-    resultList1.Add(int.Parse(GetFirstDigit(list1)));
-  }
-
-  if (list2.Contains(','))
-  {
-    var res = list2.Split(',');
-    foreach (string item in res)
-    {
-      resultList2.Add(int.Parse(item));
-    }
-  }
-  else
-  {
-    resultList2.Add(int.Parse(GetFirstDigit(list1)));
-  }
-
-  int i = 0;
-  int equal = 0;
-  while (true)
-  {
-    if (resultList1.Count == i && resultList1.Count == i && i == equal)
-    {
-      return Result.Equal;
-    }
-    if (resultList1.Count <= i)
-    {
-      return Result.Less;
-    }
-    if (resultList2.Count <= i)
-    {
-      return Result.Greater;
-    }
-
-    if (resultList1[i] < resultList2[i])
-    {
-      return Result.Less;
-    }
-
-    if (resultList1[i] > resultList2[i])
-    {
-      return Result.Greater;
-    }
-    if (resultList1[i] == resultList2[i])
-    {
-      equal++;
-    }
-    i++;
-  }
-}
-
 // 1 first < second less
 // 2 first == second qual
 // 3 first > second greater
@@ -145,7 +51,6 @@ Result CompareList(string list1, string list2)
 Result Compare(string firstList, string secondList)
 {
   Result res = Result.Less;
-  int equal = 0;
   //compare numbers
   if (!firstList.Contains('[') && !secondList.Contains("[") &&
     !firstList.Contains(']') && !secondList.Contains(']') &&
@@ -167,15 +72,6 @@ Result Compare(string firstList, string secondList)
     secondList = secondList.Remove(0, 1);
     firstList = firstList.Remove(firstList.Length - 1, 1);
     secondList = secondList.Remove(secondList.Length - 1, 1);
-    if (string.IsNullOrEmpty(firstList))
-    {
-      return Result.Less;
-    }
-    else if (string.IsNullOrEmpty(secondList))
-    {
-      return Result.Greater;
-    }
-
     while (true)
     {
       if (firstList.Length == 0 && secondList.Length != 0)
@@ -197,28 +93,24 @@ Result Compare(string firstList, string secondList)
   }
 
   //list vs nr
-  if (firstList.Count(a => a == '[') == 1 && firstList.Count(a => a == ']') == 1 &&
+  if (firstList.Contains(']') && firstList.Contains('[') &&
     !secondList.Contains(']') && !secondList.Contains('['))
   {
     res = Compare(firstList, '[' + secondList + ']');
     if (res != Result.Equal)
       return res;
-    else equal++;
   }
 
   //nr vs list
-  if (secondList.Count(a => a == '[') == 1 && secondList.Count(a => a == ']') == 1 &&
+  if (secondList.Contains(']') && secondList.Contains('[') &&
   !firstList.Contains(']') && !firstList.Contains('['))
   {
     res = Compare('[' + firstList + ']', secondList);
     if (res != Result.Equal)
       return res;
-    else equal++;
   }
   return res;
 }
-
-
 
 List<Result> result = new List<Result>();
 for (int i = 0; i < lines.Length; i += 3)
@@ -229,11 +121,12 @@ for (int i = 0; i < lines.Length; i += 3)
 int total = 0;
 for (int i = 0; i < result.Count; i++)
 {
-  if (result[i] ==  Result.Less)
+  if (result[i] == Result.Less)
   {
     total += i + 1;
   }
 }
+
 Console.WriteLine(total);
 enum Result
 {
